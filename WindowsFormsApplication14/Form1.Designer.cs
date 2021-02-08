5f3d3c109a7376b7b48e7767cfa3ba472282542c
Etibar Abdullayev <abdullayev3tibar@gmail.com>
2021-02-08 15:01:22 +0400

    Google dork Collector - first release

---
+++ WindowsFormsApplication14/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace WindowsFormsApplication14
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.buttonBasla = new System.Windows.Forms.Button();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.Location = new System.Drawing.Point(3, 51);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(661, 460);
+            this.richTextBox1.TabIndex = 9;
+            this.richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 25);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Link Sayı:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(122, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 31);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "0";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(569, 6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(84, 34);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Clean";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 2500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(488, 13);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(62, 23);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Sifirla";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(351, 6);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(105, 42);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "Stop";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // buttonBasla
+            // 
+            this.buttonBasla.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonBasla.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonBasla.Location = new System.Drawing.Point(240, 6);
+            this.buttonBasla.Name = "buttonBasla";
+            this.buttonBasla.Size = new System.Drawing.Size(105, 42);
+            this.buttonBasla.TabIndex = 8;
+            this.buttonBasla.Text = "Start";
+            this.buttonBasla.UseVisualStyleBackColor = true;
+            this.buttonBasla.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 2000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(665, 516);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.buttonBasla);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "GOOGLE SQL AXTAR";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button buttonBasla;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ WindowsFormsApplication14/Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using OpenQA.Selenium;
+using System.Net;
+
+namespace WindowsFormsApplication14
+{
+    public partial class Form1 : Form
+    {
+  
+        IWebDriver browserGOOGLE = new OpenQA.Selenium.Chrome.ChromeDriver();
+        int sayi = 0;
+        int defe = 0;
+        String currentURL;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button5_Click(object sender, EventArgs e)   
+        {
+            buttonBasla.Enabled = false;
+            button3.Enabled = true;
+            timer1.Enabled = true;
+            currentURL = browserGOOGLE.Url;
+           
+        }     
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            browserGOOGLE.Navigate().GoToUrl("https://google.com");
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                String currentURL = browserGOOGLE.Url;
+                IList<IWebElement> all = browserGOOGLE.FindElements(By.CssSelector("div[class='yuRUbf'] > a"));
+
+                StringBuilder str = new StringBuilder();
+                for (var i = 0; i < all.Count; i++)
+                {
+                    string link = all[i].GetAttribute("href");
+                    string linkdenParca = link.Substring(0, 18);
+
+                    string linkler = Convert.ToString(str);
+                    if (linkler.Contains(linkdenParca))
+                    {
+
+                    }
+                    else
+                    {
+                        if (richTextBox1.Text.Contains(linkdenParca))
+                        {
+                        }
+                        else
+                        {
+                           
+                                str.Append(link + Environment.NewLine);
+                                label2.Text = Convert.ToString(richTextBox1.Lines.Count());
+                   
+                        }
+                    }
+
+                }
+                richTextBox1.AppendText(str + "");
+                browserGOOGLE.Navigate().GoToUrl(currentURL + "&start=" + Convert.ToString(sayi));
+                sayi = sayi + 10;
+            }
+            catch
+            {
+                buttonBasla.Enabled = true;
+                button3.Enabled = false;
+                timer1.Enabled = false;
+            }
+            
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            label2.Text = Convert.ToString(0);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            sayi = 0;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            buttonBasla.Enabled = true;
+            button3.Enabled = false;
+            timer1.Enabled = false;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+           // using (var webClient = new System.Net.WebClient())
+           /// {
+           //     string result = webClient.DownloadString("http://stackoverflow.com/questions/3231969/download-file-from-url-to-a-string");
+           // }
+            timer2.Enabled = true;
+            
+            
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            Thread ty = new Thread(test);
+            ty.Start();
+        }
+       private void test()
+        {
+            defe = defe + 1;
+            var list = richTextBox1.Lines;
+            int listSayi = list.Count();
+            try
+            {
+                if (listSayi > defe)
+                {
+
+                    WebClient webb = new WebClient();
+                    string responce = webb.DownloadString(list[defe]);
+                    if (responce.Contains("localhost"))
+                    {
+                       // richTextBox2.AppendText(list[defe] + Environment.NewLine);
+                    }
+                }
+            }
+            catch { }
+        }
+        }
+    }
+
+public static class StringExt
+{
+    public static bool IsNumeric(this string text)
+    {
+        double test;
+        return double.TryParse(text, out test);
+    }
+}
